c1c45977a3a934325790e53609a5008f7bc56821
Hossein Sherkat <hosseinsherkat@gmail.com>
2025-11-14 08:20:09 +0100

    add checks for audio embeddings

--- checker.py
+++ checker.py
@@ -1,2 +1,12 @@
 import subprocess
-subprocess.run(["ffmpeg", "-version"])
+import numpy as np
+import os
+import MainCode as mc
+
+# subprocess.run(["ffmpeg", "-version"])
+
+
+emb = np.load(os.path.join(mc.audio_embeddings_path, "--0PQM4-hqg.npy"))
+print(type(emb))      # numpy.ndarray
+print(emb.shape)
+print(emb[:10]) 
