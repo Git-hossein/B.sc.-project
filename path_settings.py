61fc031280f39bd37486f8ea29da7ab1b06fe02c
Hossein <hosseinsherkat@gmail.com>
2026-03-20 15:31:21 +0100

    improve a directory's name

--- path_settings.py
+++ path_settings.py
@@ -28,7 +28,7 @@ class _PathSettings:
         self.video_embeddings_path = os.path.join(self.base_path, "video_embeddings")
         self.inferred_example_path = "./inferred_examples"
         self._TCML_server_input = os.path.join(self._TCML_server_base_path, "input")
-        self._TCML_server_output_generated = os.path.join(self._TCML_server_base_path, "output/generated_examples")
+        self._TCML_server_output_generated = os.path.join(self._TCML_server_base_path, "output/generated_audios")
 
         # Logs (same for both systems)
         self.download_and_trim_log_file = "./Logs_download_trim.csv"
